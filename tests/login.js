ff8e967230d6fbbff0d99b2614412ba137946a2f
Joey Zhen <joey@fluz.app>
2025-01-03 20:27:17 -0500

    Add mocha

--- tests/login.js
+++ tests/login.js
@@ -3,7 +3,37 @@ const LoginPage = require('../POM/loginPage');
 
 // const firefox = require('selenium-webdriver/firefox');
 
-async function cmsSearchuser() {
+
+describe("Login with https://yourlogo.geiger.com/", function() {
+  
+    it("Successfully login https://yourlogo.geiger.com/", async function(){
+        let driver = await new Builder().forBrowser("firefox").setFirefoxOptions().build();
+
+
+        const loginPage = new LoginPage(driver);
+    
+    
+        await loginPage.openUrl();
+    
+        await loginPage.login('jxz5374@rit.edu','pgswbxmouikf18j6');
+        
+        await driver.sleep(2000);
+    
+        await loginPage.rushProducts();
+    
+        
+        const finalUrl = Promise.resolve(driver.getCurrentUrl());
+    
+        finalUrl.then((value) => {
+            console.log(value);
+        })
+    
+        await driver.quit();
+
+    });
+});
+
+// async function cmsSearchuser() {
 
     // const options = new firefox.Options();
     // options.addArguments('--disable-blink-features=AutomationControlled');
@@ -14,28 +44,7 @@ async function cmsSearchuser() {
    
     // let options = new firefox.Options();
     // options.addArguments('--headless');
-    let driver = await new Builder().forBrowser("firefox").setFirefoxOptions().build();
 
+// }
 
-    const loginPage = new LoginPage(driver);
-
-
-    await loginPage.openUrl();
-
-    await loginPage.login('jxz5374@rit.edu','pgswbxmouikf18j6');
-    
-    await driver.sleep(2000);
-
-    await loginPage.rushProducts();
-
-    
-    const finalUrl = Promise.resolve(driver.getCurrentUrl());
-
-    finalUrl.then((value) => {
-        console.log(value);
-    })
-
-    await driver.quit();
-}
-
-cmsSearchuser()
+// cmsSearchuser()
