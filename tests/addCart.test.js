d4e0482ee825bc33122574e81a1445787e3eea2f
Joey Zhen <joey@fluz.app>
2025-01-11 21:28:04 -0500

    Add

--- tests/addCart.test.js
+++ tests/addCart.test.js
@@ -13,7 +13,7 @@ test("Login with https://yourlogo.geiger.com/", async function() {
     
         await loginPage.openUrl();
 
-        await driver.manage().window().setRect({ width: 2160, height: 1080 });
+        // await driver.manage().window().setRect({ width: 2160, height: 1080 });
     
         await loginPage.login('chunjingzhen@gmail.com','pgswbxmouikf18j6');
         
@@ -21,13 +21,15 @@ test("Login with https://yourlogo.geiger.com/", async function() {
 
         await driver.manage().setTimeouts({ implicit: 3000 });
 
-        await driver.findElement(By.xpath("(//img[@class='card-img-top'])[2]")).click();
+        await driver.findElement(By.xpath("(//img[@class='card-img-top'])[3]")).click();
 
         const title = Promise.resolve(await driver.findElement(By.xpath("//span[@class='text-brand']")).getText());
 
         await driver.findElement(By.xpath("(//td[@role='button'])[3]")).click();
 
         await driver.findElement(By.xpath("(//td[@role='button'])[1]")).click();
+
+        await driver.executeScript('window.scrollBy(0, 500);');
 
         await driver.manage().setTimeouts({ implicit: 3000 });
 
@@ -37,7 +39,7 @@ test("Login with https://yourlogo.geiger.com/", async function() {
        
         title.then((value) => {expect(value).toBe(expected);})
 
-        // await takeScreenshot(driver, 'final.png');
+        await takeScreenshot(driver, 'final.png');
 
         await driver.close();
 });
