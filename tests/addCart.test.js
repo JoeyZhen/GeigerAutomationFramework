919180f3a36e2b7571e5336cc3bf8a86453ac491
Joey Zhen <joey@fluz.app>
2025-02-15 20:43:06 -0500

    Add Testing

--- tests/addCart.test.js
+++ tests/addCart.test.js
@@ -27,6 +27,8 @@ test("Login with https://yourlogo.geiger.com/", async function() {
 
         await driver.findElement(By.xpath("(//td[@role='button'])[3]")).click();
 
+        await driver.manage().setTimeouts({ implicit: 2000 });
+
         await driver.findElement(By.xpath("(//td[@role='button'])[1]")).click();
 
         await driver.executeScript('window.scrollBy(0, 500);');
